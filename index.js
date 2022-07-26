e0911951368ec835d35fd4a73c3ff2023da26497
George Anastasiou <dr.george995@gmail.com>
2022-07-26 16:00:13 +0300

    basic tar

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log(process.cwd())
+
+const fs = require('fs');
+const childProcess = require('child_process');
+
+const appPath = process.cwd();
+
+// Get version
+const packageJson = require(appPath + '/package.json');
+console.log(packageJson.version);
+const version = packageJson.version;
+const appName = packageJson.name;
+
+const output = `${appName}-${version}-${Date.now()}.tar.gz`;
+
+childProcess.exec(`tar --exclude-from=.buildignore -czf ${output} *`);
+console.log(output);
